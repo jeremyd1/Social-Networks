ec8200cada78cf9e6e6987f86287b12e17f5cf41
jeremyd1 <jeremyd@berkeley.edu>
2017-10-02 17:38:18 -0700

    finished initial analysis of Village 1

--- Microfinance/Scripts/network_analysis_methods.R
+++ Microfinance/Scripts/network_analysis_methods.R
@@ -3,19 +3,6 @@ library(dplyr)
 library(magrittr)
 library(ggplot2)
 
-### find_population -> Finds the population of a village
-# 
-# ARGUMENTS
-# key_file - String - name of key file
-#
-# RETURN
-# int - number of people
-
-find_population <- function(key_file) {
-  keys <- read.csv(key_file, header = FALSE)
-  return(nrows(keys))
-}
-
 ### explore_network -> Finds characteristics of a Network
 #
 # ARGUMENTS
@@ -25,7 +12,7 @@ find_population <- function(key_file) {
 #
 # RETURN 
 # list
-# [[1]] - vector of key,degree pairs
+# [[1]] - adj matrix
 # [[2]] - data frame of degrees and their frequency
 # [[3]] - plot of degree distribution
 # [[4]] - average degree
@@ -50,16 +37,15 @@ explore_network <- function(data_file, key_file, description) {
   matrix <- as.matrix(data)
   
   # Convert adjacency matrix into an igraph object
-  igraph <- graph_from_adjacency_matrix(matrix)
+  g <- graph_from_adjacency_matrix(matrix)
   
   # Creates a numeric vector of each node's degree
-  degrees <- degree(igraph, v = V(igraph))
-  people <- names(degrees)
+  degrees <- (degree(g, v = V(g)))/2 # IN/OUT count as 1
+  id <- names(degrees)
   names(degrees) <- NULL
   
   # Create a df of nodes, degrees
-  results <- as.data.frame(degrees) %>%
-    mutate(id = people)
+  results <- as.data.frame(degrees)
   
   results_hist <- results %>%
     group_by(degrees) %>%
@@ -72,21 +58,21 @@ explore_network <- function(data_file, key_file, description) {
     ggtitle(description)
   
   # Degree Characteristics
-  avg_degree <- mean(graph$frequency) 
-  max_degree <- max(graph$frequency) 
-  min_degree <- min(graph$frequency) 
+  avg_degree <- mean(results$degrees) 
+  max_degree <- max(results$degrees) 
+  min_degree <- min(results$degrees) 
   
   # Average Path Length
-  avg_path <- mean_distance(igraph) 
+  avg_path <- mean_distance(g) 
   
   # Clustering Coefficient
-  clustering <- transitivity(igraph) 
+  clustering <- transitivity(g) 
   
   # Assortativity Coefficient based on Degrees
-  assortativity <- assortativity_degree(igraph) 
+  assortativity <- assortativity_degree(g) 
   
   # Return Values
-  values <- list(results, results_hist, plot, avg_degree, max_degree, 
+  values <- list(matrix, results_hist, plot, avg_degree, max_degree, 
                  min_degree, avg_path, clustering, assortativity)
   return(values)
 }
--- Microfinance/Scripts/village1.R
+++ Microfinance/Scripts/village1.R
@@ -1,38 +1,116 @@
-library(dplyr)
-
 ### Analysis of Village 1 Networks
 setwd("/Users/Jeremyd/Desktop/Social-Networks/Microfinance/Data")
 
-# Grab Key File
-
-
 # Village Characteristics
-population <- find_population()
-  
-  
-  
-# Networks - 12 Total
+key_file <- "network/AdjMatrixKeys/key_vilno_1.csv"
+keys <- read.csv(key_file, header = FALSE)
+population <- nrow(keys)
 
+
+# Networks - 12 Total
+elements <- c("Adjacency Matrix", "Data Frame of Degrees, Frequencies", 
+              "Plot of Degree Distribution", "Average Degree", "Max Degree",
+              "Min Degree", "Average Path Length", "Clustering Coefficient", 
+              "Assortativity Coefficient")
+adj_all = list()
 
 # Borrow Money From 
-
 data_file <- "network/AdjMatrix/adj_borrowmoney_vilno_1.csv"
-key_file <- "network/AdjMatrixKeys/key_vilno_1.csv"
 description <- "Village 1 - Degree Distribution for Borrowing Money (I)"
-BMI<- explore_network(data_file, key_file, description)
-BMI_
+BMF<- explore_network(data_file, key_file, description)
+names(BMF) <- elements
+adj_all[[length(adj_all) + 1]] = BMF[[1]]
 
 
+# Give Advice To
+data_file <- "network/AdjMatrix/adj_giveadvice_vilno_1.csv"
+description <- "Village 1 - Degree Distribution for Giving Advice (I)"
+GAT <- explore_network(data_file, key_file, description)
+names(GAT) <- elements
+adj_all[[length(adj_all) + 1]] = GAT[[1]]
 
+# Help With a Decision
+data_file <- "network/AdjMatrix/adj_helpdecision_vilno_1.csv"
+description <- "Village 1 - Degree Distribution for Help With a Decision (I)"
+HWD <- explore_network(data_file, key_file, description)
+names(HWD) <- elements
+adj_all[[length(adj_all) + 1]] = HWD[[1]]
 
+# Borrow Rice or Kerosene 
+data_file <- "network/AdjMatrix/adj_keroricecome_vilno_1.csv"
+description <- "Village 1 - Degree Distribution for Borrow Rice Or Kerosene (I)"
+BRK <- explore_network(data_file, key_file, description)
+names(BRK) <- elements
+adj_all[[length(adj_all) + 1]] = BRK[[1]]
 
+# Lend Rice or Kerosene
+data_file <- "network/AdjMatrix/adj_keroricego_vilno_1.csv"
+description <- "Village 1 - Degree Distribution for Lend Rice Or Kerosene (I)"
+LRK <- explore_network(data_file, key_file, description)
+names(LRK) <- elements
+adj_all[[length(adj_all) + 1]] = LRK[[1]]
 
+# Lend Money to 
+data_file <- "network/AdjMatrix/adj_lendmoney_vilno_1.csv"
+description <- "Village 1 - Degree Distribution for Lending Money (I)"
+LMT <- explore_network(data_file, key_file, description)
+names(LMT) <- elements
+adj_all[[length(adj_all) + 1]] = LMT[[1]]
 
+# Obtain Medical Advice from
+data_file <- "network/AdjMatrix/adj_medic_vilno_1.csv"
+description <- "Village 1 - Degree Distribution for Obtaining Medical Advice (I)"
+OMA <- explore_network(data_file, key_file, description)
+names(OMA) <- elements
+adj_all[[length(adj_all) + 1]] = OMA[[1]]
 
+# Engage Socially With
+data_file <- "network/AdjMatrix/adj_nonrel_vilno_1.csv"
+description <- "Village 1 - Degree Distribution for Social Engagement (I)"
+ESW <- explore_network(data_file, key_file, description)
+names(ESW) <- elements
+adj_all[[length(adj_all) + 1]] = ESW[[1]]
 
+# Related To
+data_file <- "network/AdjMatrix/adj_rel_vilno_1.csv"
+description <- "Village 1 - Degree Distribution for Relations (I)"
+RT <- explore_network(data_file, key_file, description)
+names(RT) <- elements
+adj_all[[length(adj_all) + 1]] = RT[[1]]
 
-
+# Go to Temple with 
 data_file <- "network/AdjMatrix/adj_templecompany_vilno_1.csv"
-key_file <- "network/AdjMatrixKeys/key_vilno_1.csv"
 description <- "Village 1 - Degree Distribution for Temple (I)"
-TI <- explore_network(data_file, key_file, description)
+TE <- explore_network(data_file, key_file, description)
+names(TE) <- elements
+adj_all[[length(adj_all) + 1]] = TE[[1]]
+
+# Invite To Home
+data_file <- "network/AdjMatrix/adj_visitcome_vilno_1.csv"
+description <- "Village 1 - Degree Distribution for Invited Home (I)"
+ITH <- explore_network(data_file, key_file, description)
+names(ITH) <- elements
+adj_all[[length(adj_all) + 1]] = ITH[[1]]
+
+# Visit in Another's Home
+data_file <- "network/AdjMatrix/adj_visitgo_vilno_1.csv"
+description <- "Village 1 - Degree Distribution for Visit Another's Home (I)"
+VAH <- explore_network(data_file, key_file, description)
+names(VAH) <- elements
+adj_all[[length(adj_all) + 1]] = VAH[[1]]
+
+
+
+### Analysis of the Specific People each Person interacts with 
+base <- adj_all[[1]]
+for (m in seq_along(adj_all)) {
+  if (m != 1) {
+    matrix <- adj_all[[m]]
+    for (i in seq(population)) {
+      for (j in seq(population)) {
+        base[i,j] = base[i,j] + matrix[i,j]
+      }
+    }
+  }
+}
+
